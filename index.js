8fc223cf0af7f769de837a550fd8f2ed76857af6
Michael Larson <larsonme@msoe.edu>
2019-02-06 21:38:51 -0600

    Implemented the getUser query and the web config to allow for the url string to be as long as it has to be.

--- index.js
+++ index.js
@@ -25,9 +25,11 @@ app.get('/index.html', function (req, res) {
         res.end();
     });
 });
-app.get('/homepage.html', function (req, res) {
-    console.log(req.query.user);
-    // checkForUser(req.query.user);
+app.get('/userId', function (req, res) {
+    checkForUser(req.query.userEmail,req.query.userName,req.query.userToken,res);
+
+});
+app.get('/homepage.html', function(req,res){
     fs.readFile('./homepage.html', function (err, data) {
         if (err) {
             throw err;
@@ -37,19 +39,18 @@ app.get('/homepage.html', function (req, res) {
         res.end();
     });
 });
-
-app.get('/permissionedPage.html', function (req, res) {
-    //Check database for userid and if they have the right permission for this page
-    //If they do, return permissionedPage
-    console.log(req.query);
-    fs.readFile('./permissionedPage.html', function (err, data) {
-        if (err) {
-            throw err;
-        }
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write(data);
-        res.end();
-    });
+// app.get('/permissionedPage.html', function (req, res) {
+//     //Check database for userid and if they have the right permission for this page
+//     //If they do, return permissionedPage
+//     console.log(req.query);
+//     fs.readFile('./permissionedPage.html', function (err, data) {
+//         if (err) {
+//             throw err;
+//         }
+//         res.writeHead(200, {"Content-Type": "text/html"});
+//         res.write(data);
+//         res.end();
+//     });
     //If they don't have the permissioned, return this
     // fs.readFile('./errorPage.html', function (err, data) {
     //     if (err) {
@@ -59,76 +60,111 @@ app.get('/permissionedPage.html', function (req, res) {
     //     res.write(data);
     //     res.end();
     // });
-});
+// });
 app.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
 
-// var config =
-//     {
-//         authentication:
-//             {
-//                 type: 'default',
-//                 options:
-//                     {
-//                         userName: '',
-//                         password: '',
-//                     }
-//             },
-//         server: 'softwarearchitecture2019.database.windows.net',
-//         options:
-//             {
-//                 database: 'spikedatabase',
-//                 encrypt: true
-//             }
-//     };
-// var connection = new Connection(config);
-// //
-// // Attempt to connect and execute queries if connection goes through
-// connection.on('connect', function (err) {
-//         if (err) {
-//             console.log(err)
-//         }
-//         else {
-//             console.log("Successfully connected");
-//         }
-//     }
-// );
-// function createUser(user){
-//     var request = new Request(
-//         //Need to return the userid here
-//         "INSERT INTO [USER] (username, displayName, email, token) VALUES('" + user.getEmail() + "'" +
-//         ", '" + user.getName() + "', '" + user.getEmail() + "', '"+ user.getToken() + "'",
-//         function(err, rowCount, rows)
-//         {
-//             console.log(rowCount + ' row(s) returned');
+var config =
+    {
+        authentication:
+            {
+                type: 'default',
+                options:
+                    {
+                        userName: 'USERNAME',
+                        password: 'PASSWORD',
+                    }
+            },
+        server: 'softwarearchitecture2019.database.windows.net',
+        options:
+            {
+                database: 'spikedatabase',
+                encrypt: true
+            }
+    };
+var connection = new Connection(config);
 //
-//             process.exit();
-//         }
-//     );
-// }
-// function checkForUser(user) {
-//     console.log('Check for User...');
-//
-//     // Read all rows from table
-//     var request = new Request(
-//         //Need to return userid from this
-//         "SELECT * FROM [USER] WHERE email = '" + user.getEmail() +"'",
-//         function(err, rowCount, rows)
-//         {
-//             console.log(rowCount + ' row(s) returned');
-//             if(rowCount == 0){
-//                 createUser(user);
-//             }
-//             process.exit();
-//         }
-//     );
-//
-//     request.on('row', function(columns) {
-//         columns.forEach(function(column) {
-//             console.log("%s\t%s", column.metadata.colName, column.value);
-//         });
-//     });
-//     connection.execSql(request);
-//
-// }
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function (err) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log("Successfully connected");
+        }
+    }
+);
+function createUser(email,name,token){
+    var request = new Request(
+        //Need to return the userid here
+        "INSERT INTO [USER] (username, displayName, email, token) VALUES('" + email + "'" +
+        ", '" + name + "', '" + email + "', '"+ token + "'",
+        function(err, rowCount, rows)
+        {
+            console.log(rowCount + ' row(s) returned');
+
+            process.exit();
+        }
+    );
+
+    var getUserRequest = new Request(
+        //Need to return userid from this
+        "SELECT * FROM [USER] WHERE email = '" + email +"'",
+        function(err, rowCount, rows)
+        {
+            var userId = 0;
+            console.log(rowCount + ' row(s) returned');
+            if(rowCount === 0){
+                userId = createUser(email,name,token);
+            }
+            process.exit();
+        }
+    );
+
+    getUserRequest.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+            if(column.metadata.colName == 'id'){
+                res.write(column.value.toString());
+                res.end();
+                console.log("Wrote out: " + column.value.toString());
+            }
+        });
+    });
+
+    connection.execSql(request);
+    connection.execSql(getUserRequest);
+
+}
+function checkForUser(email,name,token,res) {
+    console.log('Check for User...');
+
+    // Read all rows from table
+    var request = new Request(
+        //Need to return userid from this
+        "SELECT * FROM [USER] WHERE email = '" + email +"'",
+        function(err, rowCount, rows)
+        {
+            var userId = 0;
+            console.log(rowCount + ' row(s) returned');
+            if(rowCount === 0){
+                userId = createUser(email,name,token,res);
+            }
+            process.exit();
+        }
+    );
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+            if(column.metadata.colName == 'id'){
+                res.write(column.value.toString());
+                res.end();
+                console.log("Wrote out: " + column.value.toString());
+            }
+        });
+    });
+    connection.execSql(request);
+
+}
